000478d87b63deb4231852df2dd0465bd4e0da3d
Owen O'Malley <omalley@apache.org>
2009-08-27 21:46:30 +0000

    MAPREDUCE-421 Pipes need to set the exit return code when something goes wrong. (Christian Kunz via omalley)
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/mapreduce/trunk@808650 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/hadoop/mapred/pipes/Submitter.java
+++ src/java/org/apache/hadoop/mapred/pipes/Submitter.java
@@ -502,7 +502,8 @@ public class Submitter extends Configured implements Tool {
    * @param args
    */
   public static void main(String[] args) throws Exception {
-    new Submitter().run(args);
+    int exitCode =  new Submitter().run(args);
+    System.exit(exitCode);
   }
 
 }
