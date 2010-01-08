7a86001fca0a82c462ee665510dde97e03fb759a
Christopher Douglas <cdouglas@apache.org>
2010-01-08 01:49:37 +0000

    MAPREDUCE-1317. Reduce the memory footprint of Rumen objects by interning host Strings. Contributed by Hong Tang
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/mapreduce/trunk@897076 13f79535-47bb-0310-9956-ffa450edef68

--- src/tools/org/apache/hadoop/tools/rumen/LoggedTaskAttempt.java
+++ src/tools/org/apache/hadoop/tools/rumen/LoggedTaskAttempt.java
@@ -140,7 +140,7 @@ public class LoggedTaskAttempt implements DeepCompare {
   }
 
   void setHostName(String hostName) {
-    this.hostName = hostName;
+    this.hostName = hostName.intern();
   }
 
   public long getHdfsBytesRead() {
