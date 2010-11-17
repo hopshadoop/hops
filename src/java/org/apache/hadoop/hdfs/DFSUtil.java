d6bdb81aaa5e006b085342fe8296a860a6517f2f
Hairong Kuang <hairong@apache.org>
2010-11-17 05:49:32 +0000

    HDFS-1483. DFSClient.getBlockLocations returns BlockLocations with no indication that the corresponding blocks are corrupt. Contributed by Patrick Kling.
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/hdfs/trunk@1035924 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/hadoop/hdfs/DFSUtil.java
+++ src/java/org/apache/hadoop/hdfs/DFSUtil.java
@@ -220,7 +220,8 @@ public class DFSUtil {
       }
       blkLocations[idx] = new BlockLocation(names, hosts, racks,
                                             blk.getStartOffset(),
-                                            blk.getBlockSize());
+                                            blk.getBlockSize(),
+                                            blk.isCorrupt());
       idx++;
     }
     return blkLocations;
