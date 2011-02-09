b542ba6afb1aa50f88180ac1a0a015c810fc286a
Todd Lipcon <todd@apache.org>
2011-02-09 01:16:55 +0000

    HDFS-1597. Batched edit log syncs can reset synctxid and throw assertions. Contributed by Todd Lipcon.
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/hdfs/trunk@1068721 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/hadoop/hdfs/server/namenode/FSEditLog.java
+++ src/java/org/apache/hadoop/hdfs/server/namenode/FSEditLog.java
@@ -446,7 +446,6 @@ public class FSEditLog implements NNStorageListener {
     try {
       synchronized (this) {
         try {
-        assert editStreams.size() > 0 : "no editlog streams";
         printStatistics(false);
   
         // if somebody is already syncing, then wait
@@ -473,6 +472,7 @@ public class FSEditLog implements NNStorageListener {
         sync = true;
   
         // swap buffers
+        assert editStreams.size() > 0 : "no editlog streams";
         for(EditLogOutputStream eStream : editStreams) {
           try {
             eStream.setReadyToFlush();
@@ -518,8 +518,8 @@ public class FSEditLog implements NNStorageListener {
     } finally {
       // Prevent RuntimeException from blocking other log edit sync 
       synchronized (this) {
-        synctxid = syncStart;
         if (sync) {
+          synctxid = syncStart;
           isSyncRunning = false;
         }
         this.notifyAll();
@@ -954,6 +954,15 @@ public class FSEditLog implements NNStorageListener {
       return getEditFile(it.next()).lastModified();
     return 0;
   }
+
+  /**
+   * Return the txid of the last synced transaction.
+   * For test use only
+   */
+  synchronized long getSyncTxId() {
+    return synctxid;
+  }
+
 
   // sets the initial capacity of the flush buffer.
   public void setBufferCapacity(int size) {
