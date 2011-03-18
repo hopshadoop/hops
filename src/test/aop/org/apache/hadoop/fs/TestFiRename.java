0de9fbb39ef076022c5c2c387c47effb9c062a0d
Eli Collins <eli@apache.org>
2011-03-18 21:49:02 +0000

    Add import missed in HDFS-1763.
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/hdfs/trunk@1083084 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/aop/org/apache/hadoop/fs/TestFiRename.java
+++ src/test/aop/org/apache/hadoop/fs/TestFiRename.java
@@ -24,6 +24,7 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Options.Rename;
+import org.apache.hadoop.hdfs.DFSConfigKeys;
 import org.apache.hadoop.hdfs.MiniDFSCluster;
 import org.apache.hadoop.hdfs.server.namenode.FSNamesystem;
 import org.junit.After;
