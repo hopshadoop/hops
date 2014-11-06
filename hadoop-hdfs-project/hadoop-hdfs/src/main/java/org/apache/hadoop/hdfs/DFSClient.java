a330c8eba9d0c95771f7a0b7f0feed1d9de886ce
Milan Desai <milan.desai@wandisco.com>
2014-11-05 20:52:01 -0800

    [HOPS-975] HDFS-7336. Unused member DFSInputStream.buffersize. Contributed by Milan Desai.

--- hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/DFSClient.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/DFSClient.java
@@ -1398,8 +1398,7 @@ public class DFSClient implements java.io.Closeable, RemotePeerFactory,
       throws IOException, UnresolvedLinkException {
     checkOpen();
     //    Get block info from namenode
-    return new DFSInputStream(this, src, buffersize, verifyChecksum, dfsClientConf
-            .hdfsClientEmulationForSF);
+    return new DFSInputStream(this, src, verifyChecksum, dfsClientConf.hdfsClientEmulationForSF);
   }
 
   /**
