2930af82cd8e45eeaadbbe6f04b6e43d2ee56f00
Andrew Wang <wang@apache.org>
2015-02-04 11:31:31 -0800

    [HOPS-1122] HDFS-7734. Class cast exception in NameNode#main. Contributed by Yi Liu.
    (cherry picked from commit 9175105eeaecf0a1d60b57989b73ce45cee4689b)

--- hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/namenode/NameNode.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/namenode/NameNode.java
@@ -1207,8 +1207,7 @@ public class NameNode implements NameNodeStatusMXBean {
     }
 
     try {
-      StringUtils.startupShutdownMessage(NameNode.class, argv,
-          (org.apache.commons.logging.Log) LogManager.getLogger(LOG.getName()));
+      StringUtils.startupShutdownMessage(NameNode.class, argv, LOG);
       NameNode namenode = createNameNode(argv, null);
       if (namenode != null) {
         namenode.join();
