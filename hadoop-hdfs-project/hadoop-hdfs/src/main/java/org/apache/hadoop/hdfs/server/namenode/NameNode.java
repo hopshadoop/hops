31c4675d72c36c06ae756b5a96f5ec6d4d9ded4e
Akira Ajisaka <aajisaka@apache.org>
2015-02-12 17:38:37 -0800

    [HOPS-1160] HDFS-7684. The host:port settings of the deamons should be trimmed before use. Contributed by Anu Engineer.
    (cherry picked from commit 9235e2fb4e94a86671e1494f293c973cf572281c)

--- hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/namenode/NameNode.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/namenode/NameNode.java
@@ -357,8 +357,8 @@ public class NameNode implements NameNodeStatusMXBean {
    * client connection
    */
   public static InetSocketAddress getServiceAddress(Configuration conf,
-      boolean fallback) {
-    String addr = conf.get(DFS_NAMENODE_SERVICE_RPC_ADDRESS_KEY);
+                                                        boolean fallback) {
+    String addr = conf.getTrimmed(DFS_NAMENODE_SERVICE_RPC_ADDRESS_KEY);
     if (addr == null || addr.isEmpty()) {
       return fallback ? getAddress(conf) : null;
     }
@@ -493,8 +493,8 @@ public class NameNode implements NameNodeStatusMXBean {
    * @return the NameNode HTTP address
    */
   public static InetSocketAddress getHttpAddress(Configuration conf) {
-    return NetUtils.createSocketAddr(conf.get(DFS_NAMENODE_HTTP_ADDRESS_KEY,
-            DFS_NAMENODE_HTTP_ADDRESS_DEFAULT));
+    return  NetUtils.createSocketAddr(
+        conf.getTrimmed(DFS_NAMENODE_HTTP_ADDRESS_KEY, DFS_NAMENODE_HTTP_ADDRESS_DEFAULT));
   }
 
   protected void loadNamesystem(Configuration conf) throws IOException {
