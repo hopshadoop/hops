d7ad1e0afb90bed07412218fba4deaca3408b043
Jing Zhao <jing9@apache.org>
2015-03-20 10:50:03 -0700

    [HOPS-1247] HDFS-7829. Code clean up for LocatedBlock. Contributed by Takanobu Asanuma.
    (cherry picked from commit a6a5aae472d015d2ea5cd746719485dff93873a8)

--- hadoop-hdfs-project/hadoop-hdfs/src/test/java/org/apache/hadoop/hdfs/TestDFSClientRetries.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/test/java/org/apache/hadoop/hdfs/TestDFSClientRetries.java
@@ -487,8 +487,9 @@ public class TestDFSClientRetries {
       LocatedBlock goodLocatedBlock = goodBlockList.get(0);
       LocatedBlock badLocatedBlock =
           new LocatedBlock(goodLocatedBlock.getBlock(), new DatanodeInfo[]{
-              DFSTestUtil.getDatanodeInfo("1.2.3.4", "bogus", 1234)},
-              goodLocatedBlock.getStartOffset(), false);
+              DFSTestUtil.getDatanodeInfo("1.2.3.4", "bogus", 1234)
+              });
+      badLocatedBlock.setStartOffset(goodLocatedBlock.getStartOffset());
 
 
       List<LocatedBlock> badBlocks = new ArrayList<>();
