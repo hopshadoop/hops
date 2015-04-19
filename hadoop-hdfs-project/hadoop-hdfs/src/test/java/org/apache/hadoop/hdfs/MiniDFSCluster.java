2707a89740b5fa29eb6d17b25e7849377a8c79af
Tsuyoshi Ozawa <ozawa@apache.org>
2015-04-19 18:19:36 +0900

    [HOPS-1453] HDFS-8043. NPE in MiniDFSCluster teardown. Contributed by Brahma Reddy Battula.

--- hadoop-hdfs-project/hadoop-hdfs/src/test/java/org/apache/hadoop/hdfs/MiniDFSCluster.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/test/java/org/apache/hadoop/hdfs/MiniDFSCluster.java
@@ -1620,6 +1620,14 @@ public class MiniDFSCluster {
       }
     }
     UsersGroups.stop();
+    if (base_dir != null) {
+      if (deleteDfsDir) {
+        base_dir.delete();
+      } else {
+        base_dir.deleteOnExit();
+      }
+    }
+
   }
   
   /**
