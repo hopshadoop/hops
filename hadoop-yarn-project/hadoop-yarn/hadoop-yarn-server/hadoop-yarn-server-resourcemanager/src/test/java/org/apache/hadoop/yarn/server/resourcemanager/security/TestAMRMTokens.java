31ac57192ac4f8b8c0ed3f5109d20ff579ae4aa4
Antonis Kouzoupis <kouzoupis.ant@gmail.com>
2016-01-27 13:22:44 +0100

    fix failing test on windows

--- hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/src/test/java/org/apache/hadoop/yarn/server/resourcemanager/security/TestAMRMTokens.java
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/src/test/java/org/apache/hadoop/yarn/server/resourcemanager/security/TestAMRMTokens.java
@@ -47,6 +47,7 @@ import org.apache.hadoop.yarn.server.resourcemanager.TestAMAuthorization.MockRMW
 import org.apache.hadoop.yarn.server.resourcemanager.TestAMAuthorization.MyContainerManager;
 import org.apache.hadoop.yarn.server.resourcemanager.rmapp.RMApp;
 import org.apache.hadoop.yarn.server.resourcemanager.rmapp.attempt.RMAppAttempt;
+import org.apache.hadoop.yarn.server.resourcemanager.rmapp.attempt.RMAppAttemptState;
 import org.apache.hadoop.yarn.server.resourcemanager.rmapp.attempt.event.RMAppAttemptContainerFinishedEvent;
 import org.apache.hadoop.yarn.server.utils.BuilderUtils;
 import org.apache.hadoop.yarn.util.Records;
@@ -69,6 +70,7 @@ public class TestAMRMTokens {
   private static final Log LOG = LogFactory.getLog(TestAMRMTokens.class);
 
   private final Configuration conf;
+  private static final int maxWaitAttempts = 50;
 
   @Parameters
   public static Collection<Object[]> configs()
@@ -157,6 +159,16 @@ public class TestAMRMTokens {
           new RMAppAttemptContainerFinishedEvent(applicationAttemptId,
               containerStatus, new TransactionStateImpl(
               TransactionState.TransactionType.RM)));
+
+      // Make sure the RMAppAttempt is at Finished State.
+      // Both AMRMToken and ClientToAMToken have been removed.
+      int count = 0;
+      while (attempt.getState() != RMAppAttemptState.FINISHED
+              && count < maxWaitAttempts) {
+        Thread.sleep(100);
+        count++;
+      }
+      Assert.assertTrue(attempt.getState() == RMAppAttemptState.FINISHED);
 
       // Now simulate trying to allocate. RPC call itself should throw auth
       // exception.
