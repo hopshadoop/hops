d4c11d42cb566ae52bbc11c09c2cfef7bf0d71b0
Antonis Kouzoupis <kouzoupis.ant@gmail.com>
2016-01-29 10:29:28 +0100

    introduce preemption threshold in fair scheduler
    Fair scheduler base test
    
    Modify unit test for fair scheduler to extend base test
    
    Add documentation for preemption threshold
    
    Unit test for Fair scheduler preemption

--- hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/src/main/java/org/apache/hadoop/yarn/server/resourcemanager/scheduler/fair/FairSchedulerConfiguration.java
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/src/main/java/org/apache/hadoop/yarn/server/resourcemanager/scheduler/fair/FairSchedulerConfiguration.java
@@ -130,7 +130,11 @@ public class FairSchedulerConfiguration extends Configuration {
    */
   protected static final String PREEMPTION = CONF_PREFIX + "preemption";
   protected static final boolean DEFAULT_PREEMPTION = false;
-  
+
+  protected static final String PREEMPTION_THRESHOLD =
+          CONF_PREFIX + "preemption.cluster-utilization-threshold";
+  protected static final float DEFAULT_PREEMPTION_THRESHOLD = 0.8f;
+
   protected static final String PREEMPTION_INTERVAL =
       CONF_PREFIX + "preemptionInterval";
   protected static final int DEFAULT_PREEMPTION_INTERVAL = 5000;
@@ -218,6 +222,10 @@ public class FairSchedulerConfiguration extends Configuration {
 
   public boolean getPreemptionEnabled() {
     return getBoolean(PREEMPTION, DEFAULT_PREEMPTION);
+  }
+
+  public float getPreemptionUtilizationThreshold() {
+    return getFloat(PREEMPTION_THRESHOLD, DEFAULT_PREEMPTION_THRESHOLD);
   }
 
   public boolean getAssignMultiple() {
