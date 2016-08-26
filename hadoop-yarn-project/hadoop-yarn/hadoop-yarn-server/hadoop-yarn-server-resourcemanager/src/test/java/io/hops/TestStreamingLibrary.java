68d34ad0bf4a7de98899820852ffe05e32ebac00
Antonis Kouzoupis <kouzoupis.ant@gmail.com>
2016-08-26 15:17:52 +0200

    Test received events

--- hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/src/test/java/io/hops/TestStreamingLibrary.java
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/src/test/java/io/hops/TestStreamingLibrary.java
@@ -19,6 +19,7 @@ import org.apache.hadoop.net.Node;
 import org.apache.hadoop.net.NodeBase;
 import org.apache.hadoop.util.Time;
 import org.apache.hadoop.yarn.api.records.NodeId;
+import org.apache.hadoop.yarn.api.records.NodeState;
 import org.apache.hadoop.yarn.api.records.Resource;
 import org.apache.hadoop.yarn.conf.YarnConfiguration;
 import org.apache.hadoop.yarn.event.Dispatcher;
@@ -34,6 +35,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * Created by antonis on 8/24/16.
@@ -45,6 +47,11 @@ public class TestStreamingLibrary {
     private static Configuration conf;
     private static MockRM rm;
     private final int GB = 1024;
+    private int id = 0;
+
+    private int getId() {
+        return id++;
+    }
 
     @BeforeClass
     public static void setUp() throws Exception {
@@ -66,6 +73,7 @@ public class TestStreamingLibrary {
 
     @After
     public void tearDown() throws Exception {
+        TimeUnit.SECONDS.sleep(2);
         rm.stop();
     }
 
@@ -81,32 +89,138 @@ public class TestStreamingLibrary {
         LOG.debug("Heartbeat NM2");
         nm2.nodeHeartbeat(true);
 
-        TimeUnit.SECONDS.sleep(10);
+        TimeUnit.SECONDS.sleep(2);
+        nm1.nodeHeartbeat(true);
+        nm2.nodeHeartbeat(true);
+
+        TimeUnit.SECONDS.sleep(4);
     }
 
     @Test
     public void testAddNode() throws Exception {
 
-        RMNodeWriter rmNodeWriter = new RMNodeWriter(generateHopRMNode(0));
+        FullRMNode toCommit = generateHopRMNode(getId(), PendingEvent.Type.NODE_ADDED,
+                PendingEvent.Status.SCHEDULER_FINISHED_PROCESSING, NodeState.NEW);
+        RMNodeWriter rmNodeWriter = new RMNodeWriter(toCommit);
         rmNodeWriter.handle();
         LOG.debug("Persisted RMNode in DB");
 
-        TimeUnit.SECONDS.sleep(5);
-        Assert.assertEquals("There should be one RMNode", 1, rm.getRMContext().getRMNodes().size());
+        TimeUnit.SECONDS.sleep(1);
+
+        rmNodeWriter = new RMNodeWriter(generateHopRMNode(getId(), PendingEvent.Type.NODE_ADDED,
+                PendingEvent.Status.SCHEDULER_FINISHED_PROCESSING, NodeState.NEW));
+        rmNodeWriter.handle();
+        LOG.debug("Persisted dummy RMNode");
+        TimeUnit.SECONDS.sleep(1);
+
+        // Node should be added to RM nodes list
+        Assert.assertTrue("Node " + toCommit.getYarnRMNode().getNodeID(), rm.getRMContext().getRMNodes().containsKey(
+                toCommit.getYarnRMNode().getNodeID()));
+
+        // Node should be added to scheduler node list
+        Assert.assertNotNull(rm.getResourceScheduler().getNodeReport(toCommit.getYarnRMNode().getNodeID()));
+        int clusterMemory = rm.getResourceScheduler().getClusterResource().getMemory();
+        int clusterVCores = rm.getResourceScheduler().getClusterResource().getVirtualCores();
+        int numOfNodes = rm.getResourceScheduler().getNumClusterNodes();
+
+        Assert.assertEquals(toCommit.getYarnRMNode().getTotalCapability().getMemory(), clusterMemory);
+        Assert.assertEquals(toCommit.getYarnRMNode().getTotalCapability().getVirtualCores(), clusterVCores);
+        Assert.assertEquals(1, numOfNodes);
     }
 
-    private FullRMNode generateHopRMNode(int pendingEventId) {
+    @Test
+    public void testRemoveNode() throws Exception {
+        // First add a new node
+        FullRMNode addedNode = generateHopRMNode(getId(), PendingEvent.Type.NODE_ADDED,
+                PendingEvent.Status.SCHEDULER_FINISHED_PROCESSING, NodeState.NEW);
+        RMNodeWriter rmNodeWriter = new RMNodeWriter(addedNode);
+        rmNodeWriter.handle();
 
+        TimeUnit.SECONDS.sleep(1);
+
+        // Dummy insert
+        FullRMNode dummyNode = generateHopRMNode(getId(), PendingEvent.Type.NODE_ADDED,
+                PendingEvent.Status.SCHEDULER_FINISHED_PROCESSING, NodeState.NEW);
+        rmNodeWriter = new RMNodeWriter(dummyNode);
+        rmNodeWriter.handle();
+
+        TimeUnit.SECONDS.sleep(1);
+
+        // Node should be added to RM nodes list
+        Assert.assertTrue("Node " + addedNode.getYarnRMNode().getNodeID(), rm.getRMContext().getRMNodes().containsKey(
+                addedNode.getYarnRMNode().getNodeID()));
+
+        // Decommission node
+        FullRMNode decNode = generateHopRMNode(addedNode.getId(), PendingEvent.Type.NODE_REMOVED,
+                PendingEvent.Status.SCHEDULER_FINISHED_PROCESSING, NodeState.DECOMMISSIONED);
+        rmNodeWriter = new RMNodeWriter(decNode);
+        rmNodeWriter.handle();
+
+        TimeUnit.SECONDS.sleep(1);
+
+        // Dummy insert
+        rmNodeWriter = new RMNodeWriter(generateHopRMNode(getId(), PendingEvent.Type.NODE_ADDED,
+                PendingEvent.Status.SCHEDULER_FINISHED_PROCESSING, NodeState.NEW));
+        rmNodeWriter.handle();
+
+        TimeUnit.SECONDS.sleep(3);
+
+        Assert.assertNotNull(rm.getRMContext().getInactiveRMNodes().get(
+                decNode.getYarnRMNode().getNodeID().getHost()));
+        Assert.assertFalse(rm.getRMContext().getRMNodes().containsKey(decNode.getYarnRMNode().getNodeID()));
+
+        int clusterMemory = rm.getResourceScheduler().getClusterResource().getMemory();
+        int clusterVCores = rm.getResourceScheduler().getClusterResource().getVirtualCores();
+        int numOfNodes = rm.getResourceScheduler().getNumClusterNodes();
+
+        Assert.assertNull(rm.getResourceScheduler().getNodeReport(decNode.getYarnRMNode().getNodeID()));
+        Assert.assertEquals(dummyNode.getYarnRMNode().getTotalCapability().getMemory(), clusterMemory);
+        Assert.assertEquals(dummyNode.getYarnRMNode().getTotalCapability().getVirtualCores(), clusterVCores);
+        Assert.assertEquals(1, numOfNodes);
+    }
+
+    @Test
+    @Ignore
+    public void testUpdateNode() throws Exception {
+        // First add a new node
+        FullRMNode addedNode = generateHopRMNode(getId(), PendingEvent.Type.NODE_ADDED,
+                PendingEvent.Status.SCHEDULER_FINISHED_PROCESSING, NodeState.NEW);
+        RMNodeWriter rmNodeWriter = new RMNodeWriter(addedNode);
+        rmNodeWriter.handle();
+
+        TimeUnit.SECONDS.sleep(1);
+
+        // Dummy insert
+        FullRMNode dummyNode = generateHopRMNode(getId(), PendingEvent.Type.NODE_ADDED,
+                PendingEvent.Status.SCHEDULER_FINISHED_PROCESSING, NodeState.NEW);
+        rmNodeWriter = new RMNodeWriter(dummyNode);
+        rmNodeWriter.handle();
+
+        TimeUnit.SECONDS.sleep(1);
+
+        // Node should be added to RM nodes list
+        Assert.assertTrue("Node " + addedNode.getYarnRMNode().getNodeID(), rm.getRMContext().getRMNodes().containsKey(
+                addedNode.getYarnRMNode().getNodeID()));
+
+        // Update Node with newly launched containers
+    }
+    
+    private FullRMNode generateHopRMNode(int id, PendingEvent.Type type, PendingEvent.Status status,
+            NodeState state) {
 
         RMNode rmNode = new RMNodeImplDist(
-                NodeId.newInstance("localhost", 1234),
+                NodeId.newInstance("host" + id, 1234),
                 rm.getRMContext(),
-                "localhost",
+                "host" + id,
                 1337,
                 8080,
                 new NodeBase("name", "/location"),
                 Resource.newInstance(6 * GB, 6),
                 "1.0");
+
+        if (!NodeState.NEW.equals(state)) {
+            ((RMNodeImplDist) rmNode).setState(state.name());
+        }
 
         io.hops.metadata.yarn.entity.RMNode hopRMNode = new io.hops.metadata.yarn.entity.RMNode(
                 rmNode.getNodeID().toString(),
@@ -117,37 +231,49 @@ public class TestStreamingLibrary {
                 rmNode.getLastHealthReportTime(),
                 rmNode.getState().name(),
                 rmNode.getNodeManagerVersion(),
-                pendingEventId);
+                id);
 
         PendingEvent pendingEvent = new PendingEvent(rmNode.getNodeID().toString(),
-                PendingEvent.Type.NODE_ADDED,
-                PendingEvent.Status.SCHEDULER_FINISHED_PROCESSING,
-                pendingEventId);
+                type,
+                status,
+                id);
 
         io.hops.metadata.yarn.entity.Resource hopResource = new io.hops.metadata.yarn.entity.Resource(
                 rmNode.getNodeID().toString(),
                 rmNode.getTotalCapability().getMemory(),
                 rmNode.getTotalCapability().getVirtualCores(),
-                pendingEventId);
+                id);
 
         NextHeartbeat nextHB = new NextHeartbeat(rmNode.getNodeID().toString(),
-                true, pendingEventId);
+                true, id);
 
-        return new FullRMNode(hopRMNode, pendingEvent, hopResource, nextHB);
+        return new FullRMNode(rmNode, hopRMNode, pendingEvent, hopResource, nextHB, id);
     }
 
     private class FullRMNode {
+        private final int id;
+        private final RMNode yarnRMNode;
         private final io.hops.metadata.yarn.entity.RMNode rmNode;
         private final PendingEvent pendingEvent;
         private final io.hops.metadata.yarn.entity.Resource resource;
         private final NextHeartbeat nextHeartbeat;
 
-        public FullRMNode(io.hops.metadata.yarn.entity.RMNode rmNode, PendingEvent pendingEvent,
-                io.hops.metadata.yarn.entity.Resource resource, NextHeartbeat nextHeartbeat) {
+        public FullRMNode(RMNode yarnRMNode, io.hops.metadata.yarn.entity.RMNode rmNode, PendingEvent pendingEvent,
+                io.hops.metadata.yarn.entity.Resource resource, NextHeartbeat nextHeartbeat, int id) {
+            this.yarnRMNode = yarnRMNode;
             this.rmNode = rmNode;
             this.pendingEvent = pendingEvent;
             this.resource = resource;
             this.nextHeartbeat = nextHeartbeat;
+            this.id = id;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public RMNode getYarnRMNode() {
+            return yarnRMNode;
         }
 
         public io.hops.metadata.yarn.entity.RMNode getRmNode() {
