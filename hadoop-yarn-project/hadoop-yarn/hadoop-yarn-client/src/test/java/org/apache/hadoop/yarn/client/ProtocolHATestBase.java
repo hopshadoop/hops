349eec174e5a9f96c04ccabe091949f360ea6ba5
Gautier Berthou <gautier.berthou@gmail.com>
2016-10-31 14:12:15 +0100

    correct TestApplicationClientProtocolOnHA

--- hadoop-yarn-project/hadoop-yarn/hadoop-yarn-client/src/test/java/org/apache/hadoop/yarn/client/ProtocolHATestBase.java
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-client/src/test/java/org/apache/hadoop/yarn/client/ProtocolHATestBase.java
@@ -183,6 +183,8 @@ public abstract class ProtocolHATestBase extends ClientBaseWithFixes {
         (base + YarnConfiguration.DEFAULT_RM_WEBAPP_PORT));
     setConfForRM(rmId, YarnConfiguration.RM_WEBAPP_HTTPS_ADDRESS, "0.0.0.0:" +
         (base + YarnConfiguration.DEFAULT_RM_WEBAPP_HTTPS_PORT));
+    setConfForRM(rmId, YarnConfiguration.RM_GROUP_MEMBERSHIP_ADDRESS, "0.0.0.0:" +
+        (base + YarnConfiguration.DEFAULT_RM_GROUP_MEMBERSHIP_PORT));
   }
 
   @Before
