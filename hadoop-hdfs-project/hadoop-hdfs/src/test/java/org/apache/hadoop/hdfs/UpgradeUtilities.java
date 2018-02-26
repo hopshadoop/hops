1d54a27f61958f17c365c0152293e3b4afd0840b
Gautier Berthou <gautier.berthou@gmail.com>
2018-02-26 17:06:33 +0100

    [HOPS-175] apply HDFS-4693

--- hadoop-hdfs-project/hadoop-hdfs/src/test/java/org/apache/hadoop/hdfs/UpgradeUtilities.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/test/java/org/apache/hadoop/hdfs/UpgradeUtilities.java
@@ -104,6 +104,7 @@ public class UpgradeUtilities {
     config.set(DFSConfigKeys.DFS_DATANODE_DATA_DIR_KEY,
         datanodeStorage.toString());
     MiniDFSCluster cluster = null;
+    String bpid = null;
     try {
       // format data-node
       createEmptyDirs(new String[]{datanodeStorage.toString()});
@@ -137,6 +138,7 @@ public class UpgradeUtilities {
       // write more files
       writeFile(fs, new Path(baseDir, "file3"), buffer, bufferSize);
       writeFile(fs, new Path(baseDir, "file4"), buffer, bufferSize);
+      bpid = cluster.getNamesystem(0).getBlockPoolId();
     } finally {
       // shutdown
       if (cluster != null) {
@@ -148,7 +150,6 @@ public class UpgradeUtilities {
     File dnCurDir = new File(datanodeStorage, "current");
     datanodeStorageChecksum = checksumContents(DATA_NODE, dnCurDir);
     
-    String bpid = cluster.getNamesystem(0).getBlockPoolId();
     File bpCurDir =
         new File(BlockPoolSliceStorage.getBpRoot(bpid, dnCurDir), "current");
     blockPoolStorageChecksum = checksumContents(DATA_NODE, bpCurDir);
