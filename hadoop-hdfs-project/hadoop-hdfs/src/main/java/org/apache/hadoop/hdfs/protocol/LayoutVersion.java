6fc3b63047bea4930cebc57f90bd3c594bb5da47
Gautier Berthou <gautier.berthou@gmail.com>
2018-03-05 16:48:55 +0100

    [HOPS-190] apply HDFS-4339

--- hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/protocol/LayoutVersion.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/protocol/LayoutVersion.java
@@ -101,7 +101,8 @@ public class LayoutVersion {
         "Serialize block lists with delta-encoded variable length ints, " +
             "add OP_UPDATE_BLOCKS"),
     RESERVED_REL1_2_0(-41, -32, "Reserved for release 1.2.0", true, CONCAT),
-    ADD_DATANODE_AND_STORAGE_UUIDS(-42, "Replace StorageID with DatanodeUuid."
+    ADD_INODE_ID(-42, -40, "Assign a unique inode id for each inode", false),
+    ADD_DATANODE_AND_STORAGE_UUIDS(-43, "Replace StorageID with DatanodeUuid."
         + " Use distinct StorageUuid per storage directory.");
     
     final int lv;
