208fd30137bffa53eb2b11c7aa04af04a6e402a1
Gautier Berthou <gautier.berthou@gmail.com>
2018-03-06 16:36:15 +0100

    [HOPS-196] apply HDFS-4053

--- hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/DFSConfigKeys.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/DFSConfigKeys.java
@@ -303,7 +303,7 @@ public class DFSConfigKeys extends CommonConfigurationKeys {
   public static final int DFS_CLIENT_DELAY_BEFORE_FILE_CLOSE_DEFAULT = 0;
 
   public static final String DFS_BLOCK_SIZE_KEY = "dfs.blocksize";
-  public static final long DFS_BLOCK_SIZE_DEFAULT = 64 * 1024 * 1024;
+  public static final long DFS_BLOCK_SIZE_DEFAULT = 128 * 1024 * 1024;
   public static final String DFS_REPLICATION_KEY = "dfs.replication";
   public static final short DFS_REPLICATION_DEFAULT = 3;
   public static final String DFS_STREAM_BUFFER_SIZE_KEY =
