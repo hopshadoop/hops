e2520b558a32824ee7a6a90b75e088c937dd679a
Gautier Berthou <gautier.berthou@gmail.com>
2018-03-29 14:56:15 +0200

    [HOPS-302] apply HDFS-4594

--- hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/datanode/web/resources/DatanodeWebHdfsMethods.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/datanode/web/resources/DatanodeWebHdfsMethods.java
@@ -442,8 +442,9 @@ public class DatanodeWebHdfsMethods {
           throw ioe;
         }
 
-        final long n = length.getValue() != null ? length.getValue() :
-            in.getVisibleLength() - offset.getValue();
+        final long n = length.getValue() != null ?
+        Math.min(length.getValue(), in.getVisibleLength() - offset.getValue()) :
+        in.getVisibleLength() - offset.getValue();
         return Response.ok(new OpenEntity(in, n, dfsclient))
             .type(MediaType.APPLICATION_OCTET_STREAM).build();
       }
