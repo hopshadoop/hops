b7b33df67318a1537f69b336e886a899b0821df9
Gautier Berthou <gautier.berthou@gmail.com>
2018-08-10 13:45:22 +0200

    [HOPS-590] apply HDFS-6162

--- hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/namenode/FSNamesystem.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/namenode/FSNamesystem.java
@@ -4939,7 +4939,7 @@ public class FSNamesystem
       if (blockSafe < blockThreshold) {
         msg += String.format(
           "The reported blocks %d needs additional %d"
-          + " blocks to reach the threshold %.4f of total blocks %d.\n",
+          + " blocks to reach the threshold %.4f of total blocks %d.%n",
           blockSafe, (blockThreshold - blockSafe) + 1, threshold, blockTotal);
         thresholdsMet = false;
       } else {
@@ -4949,7 +4949,7 @@ public class FSNamesystem
       if (numLive < datanodeThreshold) {
         msg += String.format(
           "The number of live datanodes %d needs an additional %d live "
-          + "datanodes to reach the minimum number %d.\n",
+          + "datanodes to reach the minimum number %d.%n",
           numLive, (datanodeThreshold - numLive), datanodeThreshold);
         thresholdsMet = false;
       } else {
