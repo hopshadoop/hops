2bc019d173e90246eb645d3cb5562a8fd25d6cba
Salman Niazi <smkniazi@kth.se>
2018-10-04 15:56:24 +0200

    [HOPS-677] Parallelize -copyFromLocal

--- hadoop-common-project/hadoop-common/src/test/java/org/apache/hadoop/fs/shell/find/TestFind.java
+++ hadoop-common-project/hadoop-common/src/test/java/org/apache/hadoop/fs/shell/find/TestFind.java
@@ -17,9 +17,16 @@
  */
 package org.apache.hadoop.fs.shell.find;
 
-import static org.junit.Assert.*;
-import static org.mockito.Mockito.*;
-import static org.mockito.Matchers.*;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileStatus;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.shell.PathData;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.Timeout;
+import org.mockito.InOrder;
 
 import java.io.IOException;
 import java.io.PrintStream;
@@ -27,21 +34,8 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.LinkedList;
 
-import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.FileStatus;
-import org.apache.hadoop.fs.FileSystem;
-import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.fs.shell.PathData;
-import org.apache.hadoop.fs.shell.find.BaseExpression;
-import org.apache.hadoop.fs.shell.find.Expression;
-import org.apache.hadoop.fs.shell.find.Find;
-import org.apache.hadoop.fs.shell.find.FindOptions;
-import org.apache.hadoop.fs.shell.find.Result;
-import org.junit.Before;
-import org.junit.Rule;
-import org.junit.rules.Timeout;
-import org.junit.Test;
-import org.mockito.InOrder;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
 public class TestFind {
 
