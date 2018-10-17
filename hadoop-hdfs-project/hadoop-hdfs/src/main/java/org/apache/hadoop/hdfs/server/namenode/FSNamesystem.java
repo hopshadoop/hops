64c2a305c46fa8a5f03162af605bedf401dacc05
Gautier Berthou <gautier.berthou@gmail.com>
2018-10-17 17:34:45 +0200

    [HOPS-701] apply HDFS-5683

--- hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/namenode/FSNamesystem.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/namenode/FSNamesystem.java
@@ -6966,15 +6966,17 @@ public class FSNamesystem
         }
         boolean success = false;
         
+        String effectiveDirectiveStr = null;
         Long result = null;
         try {
           
           CacheDirectiveInfo effectiveDirective = cacheManager.addDirective(directive, pc, flags, id);
           result = effectiveDirective.getId();
+          effectiveDirectiveStr = effectiveDirective.toString();
           success = true;
         } finally {
           if (isAuditEnabled() && isExternalInvocation()) {
-            logAuditEvent(success, "addCacheDirective", null, null, null);
+            logAuditEvent(success, "addCacheDirective", effectiveDirectiveStr, null, null);
           }
           RetryCacheDistributed.setState(cacheEntry, success, PBHelper.longToBytes(result));
         }
@@ -7045,7 +7047,8 @@ public class FSNamesystem
           success = true;
         } finally {
           if (isAuditEnabled() && isExternalInvocation()) {
-            logAuditEvent(success, "modifyCacheDirective", null, null, null);
+            String idStr = "{id: " + directive.getId().toString() + "}";
+            logAuditEvent(success, "modifyCacheDirective", idStr, directive.toString(), null);
           }
           RetryCacheDistributed.setState(cacheEntry, success);
         }
@@ -7082,7 +7085,8 @@ public class FSNamesystem
           success = true;
         } finally {
           if (isAuditEnabled() && isExternalInvocation()) {
-            logAuditEvent(success, "removeCacheDirective", null, null,
+            String idStr = "{id: " + id.toString() + "}";
+            logAuditEvent(success, "removeCacheDirective", idStr, null,
                 null);
           }
           RetryCacheDistributed.setState(cacheEntry, success);
@@ -7105,7 +7109,7 @@ public class FSNamesystem
       success = true;
     } finally {
       if (isAuditEnabled() && isExternalInvocation()) {
-        logAuditEvent(success, "listCacheDirectives", null, null,
+        logAuditEvent(success, "listCacheDirectives", filter.toString(), null,
             null);
       }
     }
@@ -7131,6 +7135,7 @@ public class FSNamesystem
           return null; // Return previous response
         }
         boolean success = false;
+        String poolInfoStr = null;
         try {
           if (isInSafeMode()) {
             throw new SafeModeException(
@@ -7140,12 +7145,13 @@ public class FSNamesystem
             pc.checkSuperuserPrivilege();
           }
           CachePoolInfo info = cacheManager.addCachePool(req);
+          poolInfoStr = info.toString();
 
           success = true;
         } finally {
 
           if (isAuditEnabled() && isExternalInvocation()) {
-            logAuditEvent(success, "addCachePool", req.getPoolName(), null, null);
+            logAuditEvent(success, "addCachePool", poolInfoStr, null, null);
           }
           RetryCacheDistributed.setState(cacheEntry, success);
         }
@@ -7186,7 +7192,8 @@ public class FSNamesystem
           success = true;
         } finally {
           if (isAuditEnabled() && isExternalInvocation()) {
-            logAuditEvent(success, "modifyCachePool", req.getPoolName(), null, null);
+            String poolNameStr = "{poolName: " + req.getPoolName() + "}";
+            logAuditEvent(success, "modifyCachePool", poolNameStr, req.toString(), null);
           }
           RetryCacheDistributed.setState(cacheEntry, success);
         }
@@ -7227,7 +7234,8 @@ public class FSNamesystem
           success = true;
         } finally {
           if (isAuditEnabled() && isExternalInvocation()) {
-            logAuditEvent(success, "removeCachePool", cachePoolName, null, null);
+            String poolNameStr = "{poolName: " + cachePoolName + "}";
+            logAuditEvent(success, "removeCachePool", poolNameStr, null, null);
           }
         }
         return null;
