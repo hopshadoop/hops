f8ecda7ea84b0a646e6e2e6789f9ec606be631ca
Gautier Berthou <gautier.berthou@gmail.com>
2018-10-19 18:14:04 +0200

    [HOPS-722] apply HDFS-6268

--- hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/namenode/FSNamesystem.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/namenode/FSNamesystem.java
@@ -1340,10 +1340,10 @@ public class FSNamesystem
               blockManager.getDatanodeManager()
                   .sortLocatedBlocks(clientMachine, blocks.getLocatedBlocks());
 
+             // lastBlock is not part of getLocatedBlocks(), might need to sort it too
               LocatedBlock lastBlock = blocks.getLastLocatedBlock();
               if (lastBlock != null) {
-                ArrayList<LocatedBlock> lastBlockList =
-                    new ArrayList<>();
+                ArrayList<LocatedBlock> lastBlockList = Lists.newArrayListWithCapacity(1);
                 lastBlockList.add(lastBlock);
                 blockManager.getDatanodeManager()
                     .sortLocatedBlocks(clientMachine, lastBlockList);
