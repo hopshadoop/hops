3ee916ea0d58f39e10110882099dd42d26294a68
gautier <gautier.berthou@gmail.com>
2019-09-13 16:37:08 +0200

    [HOPS-1510] refactor NameNodeBlockTokenSecretManager

--- hadoop-hdfs-project/hadoop-hdfs/src/main/java/io/hops/metadata/security/token/block/NameNodeBlockTokenSecretManager.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/io/hops/metadata/security/token/block/NameNodeBlockTokenSecretManager.java
@@ -24,17 +24,13 @@ import io.hops.transaction.lock.LockFactory;
 import io.hops.transaction.lock.TransactionLockTypes.LockType;
 import io.hops.transaction.lock.TransactionLocks;
 import org.apache.hadoop.hdfs.security.token.block.BlockKey;
-import org.apache.hadoop.hdfs.security.token.block.BlockTokenIdentifier;
 import org.apache.hadoop.hdfs.security.token.block.BlockTokenSecretManager;
-import org.apache.hadoop.hdfs.security.token.block.DataEncryptionKey;
-import org.apache.hadoop.hdfs.security.token.block.ExportedBlockKeys;
 import org.apache.hadoop.hdfs.server.namenode.Namesystem;
-import org.apache.hadoop.util.Time;
 
 import java.io.IOException;
-import java.security.SecureRandom;
 import java.util.Collection;
 import java.util.Map;
+import org.apache.hadoop.util.Time;
 
 /**
  * Persisted version of the BlockTokenSecretManager to be used by the NameNode
@@ -53,72 +49,10 @@ public class NameNodeBlockTokenSecretManager extends BlockTokenSecretManager {
     super(true, keyUpdateInterval, tokenLifetime, blockPoolId,
         encryptionAlgorithm);
     this.namesystem = namesystem;
-    this.setSerialNo(new SecureRandom().nextInt());
-    if (isLeader()) {
-      // TODO[Hooman]: Since Master is keeping the serialNo locally, so whenever
-      // A namenode crashes it should remove all keys from the database.
-      this.generateKeys();
-    } else {
-      retrieveBlockKeys();
-    }
   }
 
   @Override
-  public void setSerialNo(int serialNo) {
-    this.serialNo = serialNo;
-  }
-
-  private void generateKeys() throws IOException {
-    if (!isMaster) {
-      return;
-    }
-    /*
-     * Need to set estimated expiry dates for currentKey and nextKey so that if
-     * NN crashes, DN can still expire those keys. NN will stop using the newly
-     * generated currentKey after the first keyUpdateInterval, however it may
-     * still be used by DN and Balancer to generate new tokens before they get a
-     * chance to sync their keys with NN. Since we require keyUpdInterval to be
-     * long enough so that all live DN's and Balancer will sync their keys with
-     * NN at least once during the period, the estimated expiry date for
-     * currentKey is set to now() + 2 * keyUpdateInterval + tokenLifetime.
-     * Similarly, the estimated expiry date for nextKey is one keyUpdateInterval
-     * more.
-     */
-    setSerialNo(serialNo + 1);
-    currentKey = new BlockKey(serialNo,
-        Time.now() + 2 * keyUpdateInterval + tokenLifetime, generateSecret());
-    currentKey.setKeyType(BlockKey.KeyType.CurrKey);
-    setSerialNo(serialNo + 1);
-    nextKey = new BlockKey(serialNo,
-        Time.now() + 3 * keyUpdateInterval + tokenLifetime, generateSecret());
-    nextKey.setKeyType(BlockKey.KeyType.NextKey);
-    addBlockKeys();
-  }
-
-  @Override
-  public ExportedBlockKeys exportKeys() throws IOException {
-    if (!isMaster) {
-      return null;
-    }
-    if (LOG.isDebugEnabled()) {
-      LOG.debug("Exporting access keys");
-    }
-    BlockKey[] allKeys = getAllKeysAndSync();
-    return new ExportedBlockKeys(true, keyUpdateInterval, tokenLifetime,
-        currentKey, allKeys);
-
-  }
-
-  @Override
-  public boolean updateKeys(final long updateTime) throws IOException {
-    if (updateTime > keyUpdateInterval) {
-      return updateKeys();
-    }
-    return false;
-  }
-
-  @Override
-  public boolean updateKeys() throws IOException {
+  public synchronized boolean updateKeys() throws IOException {
     if (!isMaster) {
       return false;
     }
@@ -126,76 +60,36 @@ public class NameNodeBlockTokenSecretManager extends BlockTokenSecretManager {
       LOG.info("Updating block keys");
       return updateBlockKeys();
     } else {
-      retrieveBlockKeys();
+      getAllKeysAndSync();
+      removeExpiredKeys();
     }
     return true;
   }
 
-  @Override
-  public DataEncryptionKey generateDataEncryptionKey() throws IOException {
-    byte[] nonce = new byte[8];
-    nonceGenerator.nextBytes(nonce);
-    BlockKey key = getBlockKeyByType(BlockKey.KeyType.CurrKey);
-
-    byte[] encryptionKey = createPassword(nonce, key.getKey());
-    return new DataEncryptionKey(key.getKeyId(), blockPoolId, nonce,
-        encryptionKey, Time.now() + tokenLifetime, encryptionAlgorithm);
+  protected boolean updateKeysParent() throws IOException {
+    return super.updateKeys();
   }
 
-  @Override
-  protected byte[] createPassword(BlockTokenIdentifier identifier) {
-    BlockKey key;
-    try {
-      key = getBlockKeyByType(BlockKey.KeyType.CurrKey);
-    } catch (IOException ex) {
-      throw new IllegalStateException(
-          "currentKey hasn't been initialized. [" + ex.getMessage() + "]");
-    }
-    if (key == null) {
-      throw new IllegalStateException("currentKey hasn't been initialized.");
-    }
-    identifier.setExpiryDate(Time.now() + tokenLifetime);
-    identifier.setKeyId(key.getKeyId());
-    if (LOG.isDebugEnabled()) {
-      LOG.debug("Generating block token for " + identifier.toString());
-    }
-    return createPassword(identifier.getBytes(), key.getKey());
-  }
-
-  @Override
-  public byte[] retrievePassword(BlockTokenIdentifier identifier)
-      throws InvalidToken {
-    if (isExpired(identifier.getExpiryDate())) {
-      throw new InvalidToken(
-          "Block token with " + identifier.toString() + " is expired.");
-    }
-    BlockKey key = null;
-    try {
-      key = getBlockKeyById(identifier.getKeyId());
-    } catch (IOException ex) {
-    }
-
-    if (key == null) {
-      throw new InvalidToken(
-          "Can't re-compute password for " + identifier.toString() +
-              ", since the required block key (keyID=" + identifier.getKeyId() +
-              ") doesn't exist.");
-    }
-    return createPassword(identifier.getBytes(), key.getKey());
-  }
-
-  public void generateKeysIfNeeded() throws IOException {
-    if (isLeader()) {
-      retrieveBlockKeys();
-      if (currentKey == null && nextKey == null) {
-        generateKeys();
+  public void initKeys() throws IOException {
+    while (true) {
+      if (isLeader()) {
+        /*
+         * If the NN is leader at this point it means that the all cluster was restarted
+         * the NN should then write its newly generated keys to the database
+         */
+        addBlockKeys();
+        return;
+      } else {
+        /*
+         * if the NN is not the leader it try to get keys until the leader as persisted keys up to date neough to
+         * the database
+         */
+        getAllKeysAndSync();
+        if (currentKey.getExpiryDate() > Time.now() + 2 * keyUpdateInterval + tokenLifetime) {
+          return;
+        }
       }
     }
-  }
-
-  private void retrieveBlockKeys() throws IOException {
-    currentKey = getBlockKeyByType(BlockKey.KeyType.CurrKey);
-    nextKey = getBlockKeyByType(BlockKey.KeyType.NextKey);
   }
 
   private void addBlockKeys() throws IOException {
@@ -215,29 +109,18 @@ public class NameNodeBlockTokenSecretManager extends BlockTokenSecretManager {
     }.handle();
   }
 
-  private BlockKey getBlockKeyById(int keyId) throws IOException {
-    return HdfsVariables.getAllBlockTokenKeysByIDLW().get(keyId);
-  }
-
-  private BlockKey getBlockKeyByType(BlockKey.KeyType keytype)
-      throws IOException {
-    return HdfsVariables.getAllBlockTokenKeysByTypeLW().get(keytype.ordinal());
-  }
-
-  private BlockKey[] getAllKeysAndSync() throws IOException {
-    BlockKey[] allKeysArr = null;
-    Collection<BlockKey> allKeys = getAllKeys();
-    if (allKeys != null) {
-      for (BlockKey key : allKeys) {
+  private synchronized void getAllKeysAndSync() throws IOException {
+    Collection<BlockKey> storedKeys = getAllKeys();
+    if (storedKeys != null) {
+      for (BlockKey key : storedKeys) {
+        allKeys.put(key.getKeyId(), key);
         if (key.isCurrKey()) {
           currentKey = key;
         } else if (key.isNextKey()) {
           nextKey = key;
         }
       }
-      allKeysArr = allKeys.toArray(new BlockKey[allKeys.size()]);
     }
-    return allKeysArr;
   }
 
   private Collection<BlockKey> getAllKeys() throws IOException {
@@ -245,51 +128,29 @@ public class NameNodeBlockTokenSecretManager extends BlockTokenSecretManager {
   }
 
   private boolean updateBlockKeys() throws IOException {
-    return (Boolean) new HopsTransactionalRequestHandler(
-        HDFSOperationType.UPDATE_BLOCK_KEYS) {
+    return (Boolean) new HopsTransactionalRequestHandler(HDFSOperationType.UPDATE_BLOCK_KEYS) {
       @Override
       public void acquireLock(TransactionLocks locks) throws IOException {
         LockFactory lf = LockFactory.getInstance();
-        locks.add(
-            lf.getVariableLock(Variable.Finder.BlockTokenKeys, LockType.WRITE));
+        locks.add(lf.getVariableLock(Variable.Finder.BlockTokenKeys, LockType.WRITE));
       }
 
       @Override
       public Object performTask() throws StorageException, IOException {
-        Map<Integer, BlockKey> keys =
-            HdfsVariables.getAllBlockTokenKeysByType();
+        Map<Integer, BlockKey> keys = HdfsVariables.getAllBlockTokenKeysByType();
         if (keys.isEmpty()) {
           if(LOG.isDebugEnabled()) {
             LOG.debug("keys is not generated yet to be updated");
           }
           return false;
         }
-        // set final expiry date of retiring currentKey
-        // also modifying this key to mark it as 'simple key' instead of 'current key'
-        BlockKey currentKeyFromDB =
-            keys.get(BlockKey.KeyType.CurrKey.ordinal());
-        currentKeyFromDB
-            .setExpiryDate(Time.now() + keyUpdateInterval + tokenLifetime);
-        currentKeyFromDB.setKeyType(BlockKey.KeyType.SimpleKey);
-
-        // after above update, we only have a key marked as 'next key'
-        // the 'next key' becomes the 'current key'
-        // update the estimated expiry date of new currentKey
-        BlockKey nextKeyFromDB = keys.get(BlockKey.KeyType.NextKey.ordinal());
-        currentKey = new BlockKey(nextKeyFromDB.getKeyId(),
-            Time.now() + 2 * keyUpdateInterval + tokenLifetime,
-            nextKeyFromDB.getKey());
-        currentKey.setKeyType(BlockKey.KeyType.CurrKey);
-
-        // generate a new nextKey
-        setSerialNo(serialNo + 1);
-        nextKey = new BlockKey(serialNo,
-            Time.now() + 3 * keyUpdateInterval + tokenLifetime,
-            generateSecret());
-        nextKey.setKeyType(BlockKey.KeyType.NextKey);
-
-        HdfsVariables
-            .updateBlockTokenKeys(currentKey, nextKey, currentKeyFromDB);
+        currentKey = keys.get(BlockKey.KeyType.CurrKey.ordinal());
+        nextKey = keys.get(BlockKey.KeyType.NextKey.ordinal());
+        int previousKeyId = currentKey.getKeyId();
+        
+        updateKeysParent();
+        
+        HdfsVariables.updateBlockTokenKeys(currentKey, nextKey, allKeys.get(previousKeyId));
         return true;
       }
     }.handle();
